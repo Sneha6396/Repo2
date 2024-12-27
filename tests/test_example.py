8a0e0b7c53db7e92f01738c4be6c879a65f60104
Sneha6396 <ikuttisneha@gmail.com>
2024-12-27 10:45:59 +0530

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,8 +1,1 @@
-# tests/test_app.py
-
-import pytest
-from app import some_function  # Example function from your app to test
-
-def test_some_function():
-    result = some_function(2, 3)
-    assert result == 5  # Check that the function works as expected
+from app.some_function import some_function
